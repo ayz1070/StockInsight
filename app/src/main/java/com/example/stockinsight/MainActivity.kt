8ae7563702ad5e2e04c39e30f7ffb777c6d5e16f
Jun <ayz1070@gmail.com>
2023-08-18 22:34:48 +0900

    바텀 네비게이션 구현

--- app/src/main/java/com/example/stockinsight/MainActivity.kt
+++ app/src/main/java/com/example/stockinsight/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.stockinsight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stockinsight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            when(it.itemId){
+                R.id.bot_home -> transaction.replace(R.id.fl_main, HomeFragment())
+                R.id.bot_bookmark -> transaction.replace(R.id.fl_main, BookmarkFragment())
+                R.id.bot_mypage -> transaction.replace(R.id.fl_main, MyPageFragment())
+            }
+            transaction.commit()
+            true
+        }
     }
 }
